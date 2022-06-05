e8a5e7867b80478db6a9e6b6cf69dc8dd2bfeab0
yongsin0129 <yongsin0129@gmail.com>
2022-06-05 16:18:31 +0800

    feat : add session

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const indexRouter = require('./modules/index')
-const usersRouter = require('./modules/users')
 
 router.use('/', indexRouter)
-router.use('/users', usersRouter)
 
 module.exports = router
